6808fa1267f6c2f7a7e344bcd134d4d143d81a6e
Jeremy <jer@jer.cx>
2019-12-23 06:16:27 -0800

    6-5 for real this time

--- 6/printable.c
+++ 6/printable.c
@@ -2,13 +2,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int allchars(FILE *fp, int (*c)(int)) {
-	while ((c = getc(fp)) != EOF)
-		if (!c)
-			return 0;
-	return 1;
-}
-
 int isprintc(int c) {
 	isprint(c);
 }
@@ -17,17 +10,36 @@ int notisprintc(int c) {
 	!isprint(c);
 }
 
+int allchars(FILE *fp, int (*pred)(int)) {
+	int c;
+	while ((c = getc(fp)) != EOF)
+		if (!pred(c))
+			return 0;
+	return 1;
+}
+
+int allcharsprint(FILE *fp) {
+	return allchars(fp, isprintc);
+}
+
+int notallcharsprint(FILE *fp) {
+	return !allcharsprint(fp);
+}
+
+
 int main(int argc, char *argv[]) {
 	int i;
 	FILE *fp;
-	int (*pred)(int) = isprintc;
+	int (*pred)(FILE *) = allcharsprint;
 
 
 	while (argc > 1 && argv[1][0] == '-') {
 		switch (argv[1][1]) {
 			case 'v':
-				pred = notisprintc;
+				pred = notallcharsprint;
+				break;
 			default:
+				printf("unrecognized option: %c\n", argv[1][1]);
 				exit(1);
 		}
 		argc--;
@@ -40,9 +52,7 @@ int main(int argc, char *argv[]) {
 			if ((fp=fopen(argv[i], "r")) == NULL) {
 				fprintf(stderr, "%s: can't open %s\n", argv[0], argv[i]);
 				exit(1);
-			} else {
-				if (allchars(fp, isprintc))
+			} else if (pred(fp))
 					printf("%s\n", argv[i]);
-			}
 	return 0;
 }
