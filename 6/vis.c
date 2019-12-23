34dca6bf5e335b0a979c391707dc5b8a3dc80586
Jeremy <jer@jer.cx>
2019-12-23 05:28:46 -0800

    6-3 vis -s n

--- 6/vis.c
+++ 6/vis.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-int main() {
-	int c, n;
+int readable(int c) {
+	return isascii(c) && (isprint(c) || c=='\n' || c=='\t' || c==' ');
+}
 
-	for (n = 0; (c = getchar()) != EOF; n++)
-	{
-		if (n == 80) {
-			n = 0;
-			putchar('\n');
-		}
-		if (!isprint(c) || c == '\\') {
-			putchar('\\');
-			switch (c) {
-				case '\\':
-					break;
-				case '\t':
-					c = 't';
-					break;
-				case '\b':
-					c = 'b';
-					break;
-				case '\r':
-					c = 'r';
-					break;
-				case '\n':
-					c = 'n';
-					break;
-				case '\f':
-					c = 'f';
-					break;
+int main(int argc, char *argv[]) {
+
+	int c, i = 0, strip = 0, enough = 0;
+	char *s;
+
+	if (argc >= 3 && strcmp(argv[1], "-s") == 0)
+		strip = atoi(argv[2]) ;
+
+	if ((s = malloc(strip)) == NULL)
+		exit(1);
+
+	while ((c = getchar()) != EOF) {
+		if (readable(c)) {
+			if (i < strip)
+				s[i++] = c;
+			else {
+				for (i = 0; i < strip; i++)
+					putchar(s[i]);
+				i = 0;
+				do {
+					putchar(c);
+				} while ((c = getchar()) != EOF && readable(c));
+
+				putchar('\n');
 			}
-		}
-		putchar(c);
+
+		} else if (!strip)
+			printf("\\%03o", c);
+		else
+			i = 0;
 	}
+
+
+	exit(0);
 }
